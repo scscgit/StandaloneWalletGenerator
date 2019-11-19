53b7a613beeadd5f7206eaf27f5851b6dbbe617f
scsc <scscgit@users.noreply.github.com>
2019-11-19 21:21:10 +0100

    Alpha version of the Bit Wallet Generator.
    As simple as it could get without sacrificing the UX too much.
    Just review the 200 code LOC and 350 designer LOC,
    and you're good to go if you want to build it yourself :)
    No dependencies, just a plain .NET Core 3.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -24,17 +24,325 @@
         #region Windows Form Designer generated code
 
         /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.mainFlowLayout = new System.Windows.Forms.FlowLayoutPanel();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.flowLayoutPanel3 = new System.Windows.Forms.FlowLayoutPanel();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.flowLayoutPanel4 = new System.Windows.Forms.FlowLayoutPanel();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button9 = new System.Windows.Forms.Button();
+            this.textBox4result = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.mainFlowLayout.SuspendLayout();
+            this.flowLayoutPanel2.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel3.SuspendLayout();
+            this.flowLayoutPanel4.SuspendLayout();
+            this.SuspendLayout();
+            //
+            // mainFlowLayout
+            //
+            this.mainFlowLayout.Controls.Add(this.flowLayoutPanel2);
+            this.mainFlowLayout.Controls.Add(this.flowLayoutPanel1);
+            this.mainFlowLayout.Controls.Add(this.flowLayoutPanel3);
+            this.mainFlowLayout.Controls.Add(this.flowLayoutPanel4);
+            this.mainFlowLayout.Controls.Add(this.label3);
+            this.mainFlowLayout.Controls.Add(this.button9);
+            this.mainFlowLayout.Controls.Add(this.textBox4result);
+            this.mainFlowLayout.Controls.Add(this.richTextBox1);
+            this.mainFlowLayout.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainFlowLayout.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.mainFlowLayout.Location = new System.Drawing.Point(0, 0);
+            this.mainFlowLayout.Name = "mainFlowLayout";
+            this.mainFlowLayout.Size = new System.Drawing.Size(819, 352);
+            this.mainFlowLayout.TabIndex = 0;
+            //
+            // flowLayoutPanel2
+            //
+            this.flowLayoutPanel2.AutoSize = true;
+            this.flowLayoutPanel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.flowLayoutPanel2.Controls.Add(this.label1);
+            this.flowLayoutPanel2.Controls.Add(this.comboBox1);
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(3, 3);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(808, 31);
+            this.flowLayoutPanel2.TabIndex = 5;
+            //
+            // label1
+            //
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular,
+                System.Drawing.GraphicsUnit.Point, ((byte) (238)));
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(599, 27);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Bit Wallet Generator for BIP39 (128 bits=12 words, 256 bits=24 words)";
+            //
+            // comboBox1
+            //
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {"128", "256"});
+            this.comboBox1.Location = new System.Drawing.Point(608, 3);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(195, 23);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            this.comboBox1.TextUpdate += new System.EventHandler(this.comboBox1_TextUpdate);
+            //
+            // flowLayoutPanel1
+            //
+            this.flowLayoutPanel1.AutoSize = true;
+            this.flowLayoutPanel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.flowLayoutPanel1.Controls.Add(this.textBox1);
+            this.flowLayoutPanel1.Controls.Add(this.button2);
+            this.flowLayoutPanel1.Controls.Add(this.button3);
+            this.flowLayoutPanel1.Controls.Add(this.button1);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 40);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(1588, 60);
+            this.flowLayoutPanel1.TabIndex = 0;
+            //
+            // textBox1
+            //
+            this.textBox1.Location = new System.Drawing.Point(3, 3);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(1580, 23);
+            this.textBox1.TabIndex = 0;
+            //
+            // button2
+            //
+            this.button2.Location = new System.Drawing.Point(3, 32);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(83, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Fill in 0";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            //
+            // button3
+            //
+            this.button3.Location = new System.Drawing.Point(92, 32);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(83, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Fill in 1";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            //
+            // button1
+            //
+            this.button1.Location = new System.Drawing.Point(181, 32);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(189, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Generate TRNG (random.org)";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            //
+            // flowLayoutPanel3
+            //
+            this.flowLayoutPanel3.AutoSize = true;
+            this.flowLayoutPanel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.flowLayoutPanel3.Controls.Add(this.textBox2);
+            this.flowLayoutPanel3.Controls.Add(this.button4);
+            this.flowLayoutPanel3.Controls.Add(this.button5);
+            this.flowLayoutPanel3.Controls.Add(this.button6);
+            this.flowLayoutPanel3.Location = new System.Drawing.Point(3, 106);
+            this.flowLayoutPanel3.Name = "flowLayoutPanel3";
+            this.flowLayoutPanel3.Size = new System.Drawing.Size(1588, 60);
+            this.flowLayoutPanel3.TabIndex = 6;
+            //
+            // textBox2
+            //
+            this.textBox2.Location = new System.Drawing.Point(3, 3);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(1580, 23);
+            this.textBox2.TabIndex = 0;
+            //
+            // button4
+            //
+            this.button4.Location = new System.Drawing.Point(3, 32);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(83, 23);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "Fill in 0";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            //
+            // button5
+            //
+            this.button5.Location = new System.Drawing.Point(92, 32);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(83, 23);
+            this.button5.TabIndex = 3;
+            this.button5.Text = "Fill in 1";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            //
+            // button6
+            //
+            this.button6.Location = new System.Drawing.Point(181, 32);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(189, 23);
+            this.button6.TabIndex = 1;
+            this.button6.Text = "Generate Offline in C#";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            //
+            // flowLayoutPanel4
+            //
+            this.flowLayoutPanel4.AutoSize = true;
+            this.flowLayoutPanel4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.flowLayoutPanel4.Controls.Add(this.textBox3);
+            this.flowLayoutPanel4.Controls.Add(this.button7);
+            this.flowLayoutPanel4.Controls.Add(this.button8);
+            this.flowLayoutPanel4.Controls.Add(this.label2);
+            this.flowLayoutPanel4.Location = new System.Drawing.Point(3, 172);
+            this.flowLayoutPanel4.Name = "flowLayoutPanel4";
+            this.flowLayoutPanel4.Size = new System.Drawing.Size(1588, 60);
+            this.flowLayoutPanel4.TabIndex = 7;
+            //
+            // textBox3
+            //
+            this.textBox3.Location = new System.Drawing.Point(3, 3);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(1580, 23);
+            this.textBox3.TabIndex = 0;
+            //
+            // button7
+            //
+            this.button7.Location = new System.Drawing.Point(3, 32);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(83, 23);
+            this.button7.TabIndex = 2;
+            this.button7.Text = "Fill in 0";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            //
+            // button8
+            //
+            this.button8.Location = new System.Drawing.Point(92, 32);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(83, 23);
+            this.button8.TabIndex = 3;
+            this.button8.Text = "Fill in 1";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            //
+            // label2
+            //
+            this.label2.Location = new System.Drawing.Point(181, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(232, 23);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Edit this part manually; trust no one ^^";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            //
+            // label3
+            //
+            this.label3.Location = new System.Drawing.Point(3, 235);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(377, 22);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Get result when ready";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            //
+            // button9
+            //
+            this.button9.Location = new System.Drawing.Point(3, 260);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(377, 25);
+            this.button9.TabIndex = 9;
+            this.button9.Text = "XOR";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            //
+            // textBox4result
+            //
+            this.textBox4result.Location = new System.Drawing.Point(3, 291);
+            this.textBox4result.Name = "textBox4result";
+            this.textBox4result.Size = new System.Drawing.Size(1580, 23);
+            this.textBox4result.TabIndex = 10;
+            this.textBox4result.Text = "Prepare all 3 inputs above before you generate the result here via XOR";
+            //
+            // richTextBox1
+            //
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Location = new System.Drawing.Point(3, 320);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(808, 20);
+            this.richTextBox1.TabIndex = 11;
+            this.richTextBox1.Text =
+                "Feel free to use the entropy in a calculator like https://iancoleman.io/bip39/ (o" +
+                "ffline) in order to get your HD BIP44 keys, or a BIP39 mnemonic seed.";
+            this.richTextBox1.LinkClicked +=
+                new System.Windows.Forms.LinkClickedEventHandler(this.richTextBox1_LinkClicked);
+            //
+            // Form1
+            //
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(819, 352);
+            this.Controls.Add(this.mainFlowLayout);
+            this.Name = "Form1";
+            this.Text = "Bit Wallet Generator";
+            this.mainFlowLayout.ResumeLayout(false);
+            this.mainFlowLayout.PerformLayout();
+            this.flowLayoutPanel2.ResumeLayout(false);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            this.flowLayoutPanel3.ResumeLayout(false);
+            this.flowLayoutPanel3.PerformLayout();
+            this.flowLayoutPanel4.ResumeLayout(false);
+            this.flowLayoutPanel4.PerformLayout();
+            this.ResumeLayout(false);
         }
 
         #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.FlowLayoutPanel mainFlowLayout;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel4;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel3;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox4result;
+        private System.Windows.Forms.RichTextBox richTextBox1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,20 +1,148 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
+using System.Diagnostics;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace StandaloneWalletGenerator
 {
     public partial class Form1 : Form
     {
+        private int _bits;
+
         public Form1()
         {
             InitializeComponent();
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ChangedBits();
+        }
+
+        private void comboBox1_TextUpdate(object sender, EventArgs e)
+        {
+            ChangedBits();
+        }
+
+        private void ChangedBits()
+        {
+            try
+            {
+                _bits = int.Parse("" == comboBox1.Text ? "0" : comboBox1.Text);
+            }
+            catch (Exception)
+            {
+                // Force validation
+                comboBox1.Text = "";
+                ChangedBits();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = new string('0', _bits);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = new string('1', _bits);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (_bits % 8 != 0)
+            {
+                textBox1.Text = "Please use bits as a multiple of 8 :)";
+                return;
+            }
+
+            try
+            {
+                textBox1.Text =
+                    new RandomOrgEntropyService().GetRandomBytes(_bits / 8)
+                        .ToList()
+                        .Select(b => Convert.ToString(b, 2).PadLeft(8, '0'))
+                        .Aggregate((a, b) => a + b);
+            }
+            catch (Exception ex)
+            {
+                textBox1.Text = ex.Message;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = new string('0', _bits);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = new string('1', _bits);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (_bits == 0)
+            {
+                textBox2.Text = "";
+                return;
+            }
+
+            if (_bits % 8 != 0)
+            {
+                textBox2.Text = "Please use bits as a multiple of 8 :)";
+                return;
+            }
+
+            var bytes = new byte[_bits / 8];
+            new Random().NextBytes(bytes);
+            textBox2.Text = bytes.ToList()
+                .Select(b => Convert.ToString(b, 2).PadLeft(8, '0'))
+                .Aggregate((a, b) => a + b);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            textBox3.Text = new string('0', _bits);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            textBox3.Text = new string('1', _bits);
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (_bits != textBox1.Text.Length
+                || textBox1.Text.Length != textBox2.Text.Length
+                || textBox2.Text.Length != textBox3.Text.Length)
+            {
+                textBox4result.Text =
+                    $"Lengths don't equal the chosen bit length {_bits} ({textBox1.Text.Length}, {textBox2.Text.Length}, {textBox3.Text.Length})";
+                return;
+            }
+
+            try
+            {
+                textBox4result.Text = "";
+                for (var i = 0; i < _bits; i++)
+                {
+                    var add = int.Parse(textBox1.Text[i].ToString())
+                              ^ int.Parse(textBox2.Text[i].ToString())
+                              ^ int.Parse(textBox3.Text[i].ToString());
+                    textBox4result.Text += add;
+                }
+            }
+            catch (Exception ex)
+            {
+                textBox4result.Text = ex.Message;
+            }
+        }
+
+        private void richTextBox1_LinkClicked(object sender, LinkClickedEventArgs e)
+        {
+            Process.Start("explorer.exe", e.LinkText);
         }
     }
 }
